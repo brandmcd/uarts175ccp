8f54cfd304ffcfef8c3f6c8bbd8b526dcf9e3601
Brandon McDonald <bamcd0313@gmail.com>
2025-03-31 13:12:17 -0400

    better but not good

--- run_project.sh
+++ run_project.sh
@@ -10,7 +10,7 @@ echo "Displaying images on Monitor 1..."
 env DISPLAY=:0 python display.py &
 
 echo "Starting slideshow on Monitor 2..."
-env DISPLAY=:0.1 python slideshow.py &
+env DISPLAY=:0 python slideshow.py &
 
 echo "Project is running!"
 
