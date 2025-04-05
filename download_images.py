38f2913f8ae50d37119c3ded635f07e8f00387fa
Brandon McDonald <brandmcd@umich.edu>
2025-04-04 20:51:43 -0400

    fix download

--- download_images.py
+++ download_images.py
@@ -49,13 +49,56 @@ def clear_leds():
     pixels.fill((0, 0, 0))
     pixels.show()
 
-def chase_sequence(color=(0, 0, 255), duration=0.05):
-    for i in range(NUM_LEDS):
-        pixels[i] = color
+def forgotten_animation():
+    end_index = random.randint(30, 50)
+    for i in range(end_index):
+        pixels[i] = (255, 255, 255)
         pixels.show()
-        time.sleep(duration)
-        pixels[i] = (0, 0, 0)
+        time.sleep(0.1)
+    time.sleep(0.5)
+    for i in reversed(range(end_index)):
+        pixels[i] = (255, 0, 0)
+        pixels.show()
+        time.sleep(0.1)
+    time.sleep(0.5)
     clear_leds()
+
+def rainbow_wheel(pos):
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    else:
+        pos -= 170
+        return (pos * 3, 0, 255 - pos * 3)
+
+def rainbow_gradient(duration=15):
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        for i in range(NUM_LEDS):
+            color = rainbow_wheel((i * 256) // NUM_LEDS)
+            pixels[i] = color
+        pixels.show()
+        time.sleep(0.05)
+    fade_out()
+
+def fade_out(steps=60):
+    for step in range(steps, -1, -1):
+        brightness = step / steps
+        for i in range(NUM_LEDS):
+            r, g, b = pixels[i]
+            pixels[i] = (int(r * brightness), int(g * brightness), int(b * brightness))
+        pixels.show()
+        time.sleep(0.05)
+    clear_leds()
+
+def remembered_animation():
+    for i in range(NUM_LEDS):
+        pixels[i] = (255, 255, 255)
+        pixels.show()
+        time.sleep(0.1)
+    rainbow_gradient()
 
 def handle_images():
     service = authenticate_drive()
@@ -63,7 +106,7 @@ def handle_images():
 
     for image in images:
         file_id = image['id']
-        file_name = safe_filename(image['name'].strip())
+        file_name = image['name']
 
         # Skip if image already seen
         seen_files = os.listdir(monitor1_path) + os.listdir(monitor2_path) + os.listdir(forgotten_path)
@@ -78,10 +121,11 @@ def handle_images():
             if random.random() < 0.5:
                 dst = os.path.join(forgotten_path, prev_file)
                 print(f"Moved {prev_file} to Forgotten")
+                forgotten_animation()
             else:
                 dst = os.path.join(monitor2_path, prev_file)
                 print(f"Moved {prev_file} to Monitor 2")
-                chase_sequence()
+                remembered_animation()
             os.rename(src, dst)
 
         # Download new image to monitor1
